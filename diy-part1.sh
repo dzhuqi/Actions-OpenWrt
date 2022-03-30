77c39b40f37ff8c792e433c0ee90cad41c11f7bb
dzhuqi <dzhuqi567@gmail.com>
2022-03-31 05:14:39 +0545

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,10 +14,10 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git small8 https://gitee.com/dzhuqi/small-package' >>feeds.conf.default
-git clone https://gitee.com/dzhuqi/pussr.git package/pussr
-#git clone https://github.com/dzhuqi/mysql.git package/mysql
+#git clone https://gitee.com/dzhuqi/pussr.git package/pussr
+git clone https://github.com/dzhuqi/mysql.git package/mysql
 
 ## 解除系统限制
 ulimit -u 10000
