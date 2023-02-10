ec1e46285802ddf77c1c8514417745d1cdf539d7
dzhuqi <dzhuqi567@gmail.com>
2023-02-10 20:11:46 +0545

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,7 +17,7 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
 #git clone https://github.com/dzhuqi/luci-app-ikoolproxy.git package/luci-app-ikoolproxy
-git clone https://github.com/dzhuqi/mysql.git package/mysql
+#git clone https://github.com/dzhuqi/mysql.git package/mysql
 git clone https://gitee.com/dzhuqi/luci-app-adguardhome package/luci-app-adguardhome
 src-git luci-app-ddns-go https://github.com/sirpdboy/luci-app-ddns-go
 
