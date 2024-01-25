f793e9522a19f7f1c97bc9525cdaae0d40c792aa
dzhuqi <dzhuqi567@gmail.com>
2024-01-25 18:13:30 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,7 +12,7 @@
 
 # Uncomment a feed source
 #sed -i "/helloworld/d" "feeds.conf.default"
-#echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
+#echo "src-git kiddin9 https://github.com/kiddin9/openwrt-packages.git" >> "feeds.conf.default"
 
 # Add a feed source
 #git clone https://github.com/liuran001/openwrt-packages package/liuran001
